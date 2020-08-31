d8b0aa02506214fb124d28fadc028a964f2ed26b
Jonathan Balls <jonathanballs@protonmail.com>
2020-08-31 11:48:23 +0100

    Part 2 complete

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 /**
  * Jonathan Balls Ticker Technical Test
  * 31 August 2020
+ * 
+ * Assumptions:
+ *  - RobotMK2 must not fall of the side of a wall. It is assumed that it may
+ *    also not fall off the bottom/go underground.
  */
 
-export class Robot {
+interface Robot {
+    x: number;
+    y: number;
+    executeInstructions: { (s: string) : void };
+}
+
+export class RobotMk1 implements Robot {
     x: number;
     y: number;
 
@@ -25,8 +35,56 @@ export class Robot {
     }
 }
 
-export function RobotControlInterface(x: number, y: number, instructions: string) {
-    let r = new Robot(x, y);
+
+export enum Orientation { F, B, L, R }
+export class RobotMk2 implements Robot {
+    x: number;
+    y: number;
+    orientation: Orientation;
+
+    constructor(x: number = 0, y: number = 0) {
+        this.x = x;
+        this.y = y;
+        this.orientation = Orientation.F;
+    }
+
+    executeInstructions(s: string) : void {
+        for (const c of s) {
+            if (c == 'L') {
+                switch(this.orientation) {
+                    case Orientation.F: this.orientation = Orientation.L; break;
+                    case Orientation.L: this.orientation = Orientation.B; break;
+                    case Orientation.B: this.orientation = Orientation.R; break;
+                    case Orientation.R: this.orientation = Orientation.F; break;
+                }
+            }
+            else if (c == 'R') {
+                switch(this.orientation) {
+                    case Orientation.F: this.orientation = Orientation.R; break;
+                    case Orientation.R: this.orientation = Orientation.B; break;
+                    case Orientation.B: this.orientation = Orientation.L; break;
+                    case Orientation.L: this.orientation = Orientation.F; break;
+                }
+            }
+            else if (c == 'F' || c == 'B') {
+                const change = (c == 'F' ? 1 : -1);
+                switch(this.orientation) {
+                    case Orientation.F: this.y += change; break;
+                    case Orientation.R: this.x += change; break;
+                    case Orientation.B: this.y -= change; break;
+                    case Orientation.L: this.x -= change; break;
+                }
+
+                if (this.x < 0) this.x = 0;
+                if (this.y < 0) this.y = 0;
+            }
+        }
+    }
+}
+
+
+export function RobotControlInterface(robot: { new(...args: any[]): Robot; }, x: number, y: number, instructions: string) {
+    let r = new robot(x, y);
     r.executeInstructions(instructions);
     return { x: r.x, y: r.y };
 }
--- src/test.ts
+++ src/test.ts
@@ -4,16 +4,30 @@
  */
 
 import * as assert from 'assert';
-import { RobotControlInterface } from './main';
+import { RobotControlInterface, RobotMk1, RobotMk2, Orientation } from './main';
 
 /**
  * Part 1 tests
  */
-describe('Robot', function() {
+describe('RobotMk1', function() {
     it('should follow instructions correctly', function() {
-        assert.deepEqual(RobotControlInterface(0, 0, 'FBLR'), { x: 0, y: 0 });
-        assert.deepEqual(RobotControlInterface(0, 0, 'FRFRFFFFFFFLLLLFFFFFRFFFFLFFLRRF'), { x: 21, y: 1 });
-        assert.deepEqual(RobotControlInterface(3, 6, 'FFFFFFFFRRRRRRRFFFFLLLBBRRRRRLLLLLLLLLRFFF'), { x: 16, y: 5 });
-        assert.deepEqual(RobotControlInterface(0, 7, 'RRRRRRRRFFFFFFFFFFFLLLBBBBBRRRLLLLLFFLR'), { x: 8, y: 4 });
+        assert.deepEqual(RobotControlInterface(RobotMk1, 0, 0, 'FBLR'), { x: 0, y: 0 });
+        assert.deepEqual(RobotControlInterface(RobotMk1, 0, 0, 'FRFRFFFFFFFLLLLFFFFFRFFFFLFFLRRF'), { x: 21, y: 1 });
+        assert.deepEqual(RobotControlInterface(RobotMk1, 3, 6, 'FFFFFFFFRRRRRRRFFFFLLLBBRRRRRLLLLLLLLLRFFF'), { x: 16, y: 5 });
+        assert.deepEqual(RobotControlInterface(RobotMk1, 0, 7, 'RRRRRRRRFFFFFFFFFFFLLLBBBBBRRRLLLLLFFLR'), { x: 8, y: 4 });
     })
+});
+
+describe('RobotMk2', function() {
+    it('should follow instructions correctly', function() {
+        let r = new RobotMk2();
+        assert.deepEqual(RobotControlInterface(RobotMk2, 0, 0, 'FFFF'), { x: 0, y: 4 });
+        assert.deepEqual(RobotControlInterface(RobotMk2, 0, 0, 'FFRF'), { x: 1, y: 2 });
+        assert.deepEqual(RobotControlInterface(RobotMk2, 0, 0, 'LLLF'), { x: 1, y: 0 });
+        assert.deepEqual(RobotControlInterface(RobotMk2, 0, 0, 'RRRBBB'), { x: 3, y: 0 });
+    })
+
+    it('should not fall off the wall', function() {
+        assert.deepEqual(RobotControlInterface(RobotMk2, 0, 0, 'LF'), { x: 0, y: 0 });
+    });
 });
